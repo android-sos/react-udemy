ec5866aa33ab8805b45aa2971e4a5105c3ca1eed
samuel <samuel.ospina36@gmail.com>
2018-10-15 22:16:13 -0500

    BurgerIngrdient component refactor

--- Projects/src/components/Burger/BurgerIngredients/burgerIngredients.js
+++ Projects/src/components/Burger/BurgerIngredients/burgerIngredients.js
@@ -1,56 +1,65 @@
-import React from 'react'
+import React, { Component} from 'react'
+import ProTypes from 'prop-types'
 import Classes from './burgerIngrdients.css'
-const burgerIngrdients = (props) => {
-    let ingredient = null;
 
-    switch(props.ingredient)
-    {
-        case "bread-bottom":
+
+ class BurgerIngrdients extends Component{
+    render () {
+        let ingredient = null;
+
+        switch(this.props.ingredient)
         {
-            ingredient = <div className={Classes.BreadBottom}> </div>;
+            case "bread-bottom":
+            {
+                ingredient = <div className={Classes.BreadBottom}> </div>;
+            }
+            break;
+    
+            case "bread-top":
+            {
+                ingredient = (
+                 <div className={Classes.BreadTop}>
+                    <div className={Classes.Seeds1}></div>
+                    <div className={Classes.Seeds1}></div>    
+                 </div>
+                );
+            }
+            break;
+    
+            case "meat":
+            {
+                ingredient = <div className={Classes.Meat}> </div>;
+            }
+            break;
+    
+            case "chesse":
+            {
+                ingredient = <div className={Classes.Chesse}> </div>;
+            }
+            break;
+    
+            case "bacon":
+            {
+                ingredient = <div className={Classes.Bacon}> </div>;
+            }
+            break;
+    
+            case "salad":
+            {
+                ingredient = <div className={Classes.Salad}> </div>;
+            }
+            break;
+            
+            default:
+              ingredient = null;
+    
         }
-        break;
-
-        case "bread-top":
-        {
-            ingredient = (
-             <div className={Classes.BreadTop}>
-                <div className={Classes.Seeds1}></div>
-                <div className={Classes.Seeds1}></div>    
-             </div>
-            );
-        }
-        break;
-
-        case "meat":
-        {
-            ingredient = <div className={Classes.Meat}> </div>;
-        }
-        break;
-
-        case "chesse":
-        {
-            ingredient = <div className={Classes.Chesse}> </div>;
-        }
-        break;
-
-        case "bacon":
-        {
-            ingredient = <div className={Classes.Bacon}> </div>;
-        }
-        break;
-
-        case "salad":
-        {
-            ingredient = <div className={Classes.Salad}> </div>;
-        }
-        break;
-        
-        default:
-          ingredient = null;
-
+    
+        return ingredient;
     }
-
-    return ingredient;
 }
-export default burgerIngrdients;
+
+BurgerIngrdients.protoTypes = {
+    type: ProTypes.string.isRequired
+}
+export default BurgerIngrdients;
