56bc6ad670c8f6dcee5cb744f6cedef179524123
samuel <samuel.ospina36@gmail.com>
2018-11-03 22:59:34 -0500

    Started Toolbar

---
+++ Projects/src/components/Navegation/Toolbar/Toolbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const toolbar = ()=> (
+    <header>
+        <div>TOOLBAR</div>
+        <div>LOGO</div>
+        <div>MENU</div>
+    </header>
+    
+);
+
+export default toolbar;
