53ce2bd4cf605e1e2f65fda5282a8a08fbe64bb0
samuel <samuel.ospina36@gmail.com>
2018-11-05 23:12:28 -0500

    toolba creates

--- Projects/src/components/Navegation/Toolbar/Toolbar.js
+++ Projects/src/components/Navegation/Toolbar/Toolbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import classes from './Toolbar.css'
 const toolbar = ()=> (
-    <header>
+    <header className={classes.Toolbar}>
         <div>TOOLBAR</div>
         <div>LOGO</div>
         <div>MENU</div>
